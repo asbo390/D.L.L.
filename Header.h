419bf60fa3f9563aa7bfc61bb707fab18ca27e57
os390 <30366042+os390@users.noreply.github.com>
2017-11-10 14:13:48 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,120 @@
+#ifndef Header_h
+#define Header_h
+using namespace std;
+struct flight {
+    int numb;
+    string destination;
+    float time;
+    int cost;
+    int free_spots;
+    void get (void) {
+        cout<<"Enter Flight Number: "<<endl;
+        cin>>numb;
+        cout<<"Enter Destination: "<<endl;
+        cin>>destination;
+        cout<<"Set Arrival Time: "<<endl;
+        cin>>time;
+        cout<<"Set Cost: "<<endl;
+        cin>>cost;
+        cout<<"Set available seats: "<<endl;
+        cin>>free_spots;
+        cout<<"______________________________________________\n"<<endl;
+    }
+    void printflight (void) {
+
+        cout<<"Flight Number is "<<numb<<endl;
+        cout<<"Destination is "<<destination<<endl;
+        cout<<"Arrival Time is "<<time<<endl;
+        cout<<"Cost is "<<cost<<endl;
+        cout<<"There are "<<free_spots<<" Available Seats"<<endl;
+        cout<<"______________________________________________\n"<<endl;
+    }
+};
+struct Node
+{
+    flight data;
+    Node *N,*P;
+    Node(flight y)
+    {
+        data = y;
+        N = P = NULL;
+    }
+};
+
+class List
+{
+    Node *front;
+    Node *back;
+public:
+    List() {
+        front = NULL;
+        back = NULL;
+        
+    }
+    ~List() {
+        
+    }
+    void AddElement(flight,int,char*);
+    void Sell(int,int);
+    void DeleteElement(void);
+    void ShowElements_str(void);
+    void ShowElements_bck(void);
+    void Search(void);
+};
+
+void List::AddElement(flight x,int f,char* filename) {
+    Node *n = new Node(x);
+    if( front == NULL)
+    {
+        front = n;
+        back = n;
+    }
+    else
+    {
+        front->P = n;
+        n->N = front;
+        front = n;
+    }
+
+    if (f==1) { 
+        ofstream myfile;
+        flight data;
+        myfile.open(filename,ios::binary | ios::in | ios_base::app);
+        if (myfile.is_open())
+        {
+            myfile<<x.numb<<" ";
+            myfile<<x.destination<<" ";;
+            myfile<<x.time<<" ";
+            myfile<<x.cost<<" ";
+            myfile<<x.free_spots<<endl;
+        }
+    }
+    
+}
+void List::ShowElements_str(void) {
+    if (front==NULL && back==NULL) {
+        cout<<endl<<"List is empty"<<endl;
+        return ;
+    }
+    Node *temp = front;
+    cout << "\nList in forward order:" << endl;
+    while(temp != NULL)
+    {
+        temp->data.printflight();
+        temp = temp->N;
+    }
+}
+void List::ShowElements_bck() {
+    
+}
+void List::Sell(int a,int b) {
+    
+}
+void List::DeleteElement() {
+    
+}
+void List::Search() {
+    
+}
+
+#endif /* Header_h */
---
+++ main.cpp
@@ -0,0 +1,82 @@
+//
+//  main.cpp
+//  Lab1_T1_2.0
+//
+//  Created by Александр on 09/11/2017.
+//  Copyright © 2017 11. All rights reserved.
+//
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include "Header.h"
+using namespace std;
+
+int menu (void) {
+    int r=0;
+    cout<<"Functions: "<<endl<<"1.Add element"<<endl<<"2.Delete element"<<endl<<"3.Show elements in streight order"<<endl<<"4.Show elements in backward order"<<endl<<"5.Search by flight number"<<endl<<"6.Sell Tickets"<<endl<<"7.Exit"<<endl;
+    cin>>r;
+    return (r);
+}
+int main(int argc, char** argv) {
+    char filename[40];
+    if(argc >= 2)
+        strcat(strcpy(filename, argv[1]), ".dat");
+    else
+
+    {
+        cout << "Enter File name:" << endl;
+        cin >> filename;
+        strcat(filename, ".dat");
+    }
+    List mylist;
+    ifstream myfile;
+    flight data;
+    int filecheck=0;
+    myfile.open(filename,ios::binary | ios::out);   //Заполнение листа из файла
+    if (myfile.is_open())
+    {
+        if ( myfile.peek()==ifstream::traits_type::eof() ){
+            cout<<"File is empty"<<endl;
+            filecheck=1;
+        }
+        if(filecheck==0) {
+        while (!myfile.eof()){
+            myfile>>data.numb;
+            myfile>>data.destination;
+            myfile>>data.time;
+            myfile>>data.cost;
+            myfile>>data.free_spots;
+            mylist.AddElement(data,0,filename);
+        }
+        }
+    }
+    else {
+        cout << "Unable to open file";
+        return 0;
+    }
+    int r;
+    do{
+        r=menu();
+        switch(r)
+        {
+            case 1:
+                data.get();
+                mylist.AddElement(data,1,filename);
+                break;
+            case 2: mylist.DeleteElement(); break;
+            case 3: mylist.ShowElements_str(); break;
+            case 4: mylist.ShowElements_bck(); break;
+            case 5: mylist.Search(); break;
+            case 6:
+                int a;
+                int b;
+                cin>>a;
+                cin>>b;
+                mylist.Sell(a,b);
+                break;
+        }
+    cout<<"______________________________________________\n"<<endl;
+    } while (r!=7);
+    
+    return 0;
+}
